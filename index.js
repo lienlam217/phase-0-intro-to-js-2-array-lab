d84ff0c0001d487cfa52f54ae7ef959027a0a5ba
Lien Nguyen <lienlam217@gmail.com>
2022-10-26 21:07:04 -0500

    commit tree b97a25b1cb0d49e1a21350c7dcbaeefdb49e12e3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+ 
+cats;
+
+function destructivelyAppendCat(n) {
+    cats.push(n);
+}
+function destructivelyPrependCat(n) {
+    cats.unshift(n);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift() 
+}
+
+
+function appendCat(name) {
+   return ([...cats, name])
+
+}
+ 
+function prependCat(name) {
+    return ([name, ...cats])
+}
+
+function removeLastCat() {
+    return (cats.slice(0, 2))
+}
+
+function removeFirstCat() {
+    return (cats.slice(1))
+}
